314fd4b4c954f4524aec28c5771c9067bb428426
tcisio10 <63074263+tcisio10@users.noreply.github.com>
2020-04-05 12:58:59 -0300

    Atividade aula 06
    atividade aula 06

--- atividade06.java
+++ atividade06.java
@@ -1,11 +1,22 @@
 public class MyClass {
     public static void main(String args[]) {
-  int a1 = 1;
-  int an = 30;
-  int n = 30;
-  
-  int resultado = ((a1+an)*n)/2;
-  
-  System.out.println("A soma da PA é " + resultado);
+     
+     int [] PA = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 , 11 , 12 , 13 , 14 , 15 , 16 , 17 ,18 ,19 ,20 ,21 ,22 ,23 ,24 ,25 ,26 ,27 ,28 ,29 ,30 };
+     int soma = 0;
+    
+     
+     for (int i=0; i < PA.length; i++){
+     soma = soma + PA[i];
+     System.out.println("total: " +soma);
+         
+         
+             
+     } 
+     
+     
+       
+     
+     
+     
     }
 }
